97e552f3f20727a6b2626c7897374a549efadbb8
jjmalf07 <jjmalf07@gmail.com>
2014-05-04 17:09:01 -0500

    Part 1 Submission

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -8,6 +8,8 @@ class ActorsController < ApplicationController
     # Use the information after the slash in the URL to look up the actor with the corresponding ID number
     # Store the actor in the @actor instance variable so the view can format it
 
-    # @actor = ???
+    @id = params[:id]
+
+    @actor = Actor.find_by({ :id => params[:id]})
   end
 end
